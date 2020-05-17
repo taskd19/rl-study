fc054eea936dcf307bdb162df23ac091df2bc47c
taskd19 <taskd19@gmail.com>
2020-05-17 18:12:20 +0900

    Add doctest to Action.

--- lib/mdp/action.ex
+++ lib/mdp/action.ex
@@ -3,22 +3,41 @@ defmodule RlStudy.MDP.Action do
   ref: https://gist.github.com/smpallen99/9995893
   """
 
+  @doc """
+      iex> RlStudy.MDP.Action.up()
+      :up
+  """
+  @spec up :: :up
   def up() do
     :up
   end
 
+  @spec down :: :down
   def down() do
     :down
   end
 
+  @spec left :: :left
   def left() do
     :left
   end
 
+  @spec right :: :right
   def right() do
     :right
   end
 
+  @doc """
+      iex> RlStudy.MDP.Action.opsite_action(:up)
+      :down
+      iex> RlStudy.MDP.Action.opsite_action(:down)
+      :up
+      iex> RlStudy.MDP.Action.opsite_action(:left)
+      :right
+      iex> RlStudy.MDP.Action.opsite_action(:right)
+      :left
+  """
+  @spec opsite_action(:down | :left | :right | :up) :: :down | :left | :right | :up
   def opsite_action(action) do
     case action do
       :up -> :down
